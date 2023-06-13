e12276d499436c75341b42d08a2ffd57b13a1dbc
Jesse Jurman <j.r.jurman@gmail.com>
2023-06-13 14:42:48 -0400

    update to use latest tram-lite, new color scheme

--- calculator.js
+++ calculator.js
@@ -5,31 +5,61 @@ const calcState = {
 	tape: [],
 };
 
+const calcTape = document.querySelector('calc-tape');
+const calcDisplay = document.querySelector('calc-display');
+
+define`
+	<calc-display memory="0" operator="+" value="0" eval="0">
+		<style>
+			span[memory],
+			span[preview] {
+				opacity: 0.8;
+			}
+		</style>
+		<span memory>${'memory'}</span>
+		<span operator>${'operator'}</span>
+		<span value>${'value'}</span>
+		<span preview> = ${'eval'}</span>
+	</calc-display>
+`;
+
+define`
+	<calc-tape tape="[]">
+		<style>
+			section {
+				opacity: 0.8;
+			}
+		</style>
+		<section>
+		</section>
+	</calc-tape>
+`;
+
 const currentEval = () => {
 	return eval(`${calcState.memory} ${calcState.operator} ${calcState.value}`);
 };
 
 const updateDisplay = () => {
-	document.querySelector('calc-value').innerHTML = calcState.value;
-	document.querySelector('calc-operator').innerHTML = calcState.operator;
-	document.querySelector('calc-memory').innerHTML = calcState.memory;
-	document.querySelector('calc-tape').innerHTML = calcState.tape.join('<br />');
-	document.querySelector('calc-preview').innerHTML = `= ${currentEval()}`;
+	calcDisplay.setAttribute('value', calcState.value);
+	calcDisplay.setAttribute('operator', calcState.operator);
+	calcDisplay.setAttribute('memory', calcState.memory);
+	calcDisplay.setAttribute('eval', currentEval());
+	calcTape.setAttribute('tape', JSON.stringify(calcState.tape));
 };
 
-updateDisplay();
+addAttributeListener(calcTape, 'tape', () => {
+	const [tapeRoll] = queryAllDOM('section', calcTape);
+	tapeRoll.innerHTML = '';
+	const tape = JSON.parse(calcTape.getAttribute('tape'));
+	tape.forEach((tapeRow) => tapeRoll.append(html`<div>${tapeRow}</div>`));
+});
 
-const onPressDigit = (num) => () => {
+const onPressDigit = (num) => {
 	calcState.value = `${parseFloat(calcState.value + num)}`;
 	updateDisplay();
 };
 
-['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].forEach((digit) => {
-	const calcButton = html` <button digit style="grid-area: but${digit};" onclick=${onPressDigit(digit)}>${digit}</button> `;
-	document.querySelector('calc-controls').appendChild(calcButton);
-});
-
-const onPressOperator = (operator) => () => {
+const onPressOperator = (operator) => {
 	const result = currentEval();
 	calcState.tape.push(`${calcState.memory} ${calcState.operator} ${calcState.value} = ${result}`);
 	calcState.memory = result;
@@ -39,14 +69,6 @@ const onPressOperator = (operator) => () => {
 	updateDisplay();
 };
 
-[{value: '+', key: 'plus'}, {value: '-', key: 'minu'}, {value: '/', key: 'divi'}, {value: '*', key: 'mult'}].forEach(({value: operator, key}) => {
-	const operatorButton = html`<button operator style="grid-area: ${key}" onclick=${onPressOperator(operator)}>${operator}</button>`;
-	document.querySelector('calc-controls').appendChild(operatorButton);
-});
-
-const equalButton = html`<button equal style="grid-area: equa" onclick=${onPressOperator('+')}>=</button>`;
-document.querySelector('calc-controls').appendChild(equalButton);
-
 const onPressClear = () => {
 	calcState.memory = '0';
 	calcState.operator = '+';
@@ -54,25 +76,16 @@ const onPressClear = () => {
 	updateDisplay();
 };
 
-const clearButton = html`<button clear style="grid-area: clea;" onclick=${onPressClear}>Clear</button>`;
-document.querySelector('calc-controls').appendChild(clearButton);
-
 const onPressBackspace = () => {
 	calcState.value = '0';
 	updateDisplay();
 };
-
-const backspaceButton = html`<button clear style="grid-area: back;" onclick=${onPressBackspace}>⌫</button>`;
-document.querySelector('calc-controls').appendChild(backspaceButton);
 
 const onPressPoint = () => {
 	if (calcState.value.includes('.')) {
 		// do nothing
 		return;
 	}
-	calcState.value = calcState.value + '.'
-	updateDisplay()
-}
-
-const decimalPointButton = html`<button digit style="grid-area: poin;" onclick=${onPressPoint}>.</button>`;
-document.querySelector('calc-controls').appendChild(decimalPointButton);
+	calcState.value = calcState.value + '.';
+	updateDisplay();
+};
